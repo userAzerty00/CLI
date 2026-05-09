0bd050fe9323570692240210798d501778dbdc81
M31i55a <melysgioda34@gmail.com>
2026-05-09 06:30:11 +0000

    added the Err handling for the 'run' case

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,15 +15,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
-        fs::read_to_string(&config.filename)
-            .expect("Something went wrong reading the file");
+        fs::read_to_string(&config.filename)?;
 
     println!("With text: \n{}", contents);
+    Ok(())
 }
 
 struct Config {
