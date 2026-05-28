33ac92e1e77ae2eabf244af103f2fd65d1dc94ab
M31i55a <melysgioda34@gmail.com>
2026-05-28 08:26:50 +0000

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use rust_project::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,35 +15,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rust_project::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents =
-        fs::read_to_string(&config.filename)?;
-
-    println!("With text: \n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    // the new function is a convention for a constructor in Rust
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
